1e70497e68a48a3fb9fac9b179d86fdcd5375a7e
lantaris <edik.ponomarenko@mail.ru>
2023-03-23 13:59:00 +0300

    Add call funx(rest)

--- rest/rest_call.go
+++ rest/rest_call.go
@@ -14,57 +14,8 @@ type Call struct {
 	Retry int
 }
 
-// ********************************************************
-func (SELF *Call) POST(URL string, data []byte) (rbody []byte, err error) {
-
-	fmtlog.Debugln("Rest call POST:[" + URL + "] ")
-	for RetryCnt := 1; RetryCnt <= SELF.Retry; RetryCnt++ {
-		resp, err := http.Post(URL, "application/json", bytes.NewBuffer(data))
-		if err != nil {
-			fmtlog.Errorln("Error call to [" + URL + "]. Retry...  " + err.Error())
-			continue
-		}
-
-		rbody, err = ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmtlog.Errorln("Error read data from [" + URL + "]. Retry...  " + err.Error())
-			continue
-		}
-
-		return rbody, nil
-	}
-	err = errors.New("Rest call to [" + URL + "] retry exceeded")
-	fmtlog.Errorln(err.Error())
-	return nil, err
-}
-
-// ********************************************************
-func (SELF *Call) GET(URL string) (rbody []byte, err error) {
-
-	fmtlog.Debugln("Rest call to GET:[" + URL + "] ")
-
-	for RetryCnt := 1; RetryCnt <= SELF.Retry; RetryCnt++ {
-		resp, err := http.Get(URL)
-		if err != nil {
-			fmtlog.Errorln("Error call to [" + URL + "]. Retry...  " + err.Error())
-			continue
-		}
-
-		rbody, err = ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmtlog.Errorln("Error read data from [" + URL + "]. Retry...  " + err.Error())
-			continue
-		}
-
-		return rbody, nil
-	}
-	err = errors.New("Rest call to [" + URL + "] retry exceeded")
-	fmtlog.Errorln(err.Error())
-	return nil, err
-}
-
 // ======================================================================
-func (SELF *Call) post(url string, header http.Header, body io.Reader) (ResHeader http.Header, ResData []byte, err error) {
+func (SELF *Call) Exec(url string, header http.Header, body io.Reader) (ResHeader http.Header, ResData []byte, err error) {
 	var (
 		req    *http.Request
 		client *http.Client
