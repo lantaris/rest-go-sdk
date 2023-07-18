679d281da855e942de76d54b8fff8b62908aab17
lantaris <edik.ponomarenko@mail.ru>
2023-07-18 22:31:23 +0300

    Add postgres support

--- orm/go.mod
+++ orm/go.mod
@@ -2,10 +2,10 @@ module github.com/lantaris/rest-go-sdk/orm
 
 go 1.15
 
-
 require (
-        github.com/lantaris/rest-go-sdk/fmtlog v0.0.0-20230323105900-1e70497e68a4
-        gorm.io/driver/mysql v1.4.6 // indirect
-        gorm.io/gorm v1.23.8 // indirect
+	github.com/lantaris/rest-go-sdk/fmtlog v0.0.0-20230323105900-1e70497e68a4
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	gorm.io/driver/mysql v1.4.6
+	gorm.io/driver/postgres v1.5.2
+	gorm.io/gorm v1.25.0
 )
-
--- orm/orm_init_db.go
+++ orm/orm_init_db.go
@@ -4,17 +4,18 @@ import (
 	"github.com/lantaris/rest-go-sdk/fmtlog"
 	"fmt"
 	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 // ***********************************************************************
-func (SELF *TORM) Init(Host string, Database string, Username string, Password string) error {
+func (SELF *TORM) Init(DBtype string, Host string, Database string, Username string, Password string) error {
 	var (
 		err error = nil
 	)
 
 	// Connect to database
-	err = SELF.Connect(Host, Database, Username, Password)
+	err = SELF.Connect(DBtype, Host, Database, Username, Password)
 	if err != nil {
 		return err
 	}
@@ -23,11 +24,12 @@ func (SELF *TORM) Init(Host string, Database string, Username string, Password s
 }
 
 // ***********************************************************************
-func (SELF *TORM) Connect(Host string, Database string, Username string, Password string) error {
+func (SELF *TORM) Connect(DBtype string, Host string, Database string, Username string, Password string) error {
 	var (
 		err error = nil
 		db_host, db_username,
-		db_password, db_database, dsn string
+		db_password, db_database,
+		dsn string
 	)
 
 	// Get database configuration
@@ -36,11 +38,25 @@ func (SELF *TORM) Connect(Host string, Database string, Username string, Passwor
 	db_username = Username
 	db_password = Password
 
-	dsn = fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local", db_username, db_password, db_host, db_database)
-	SELF.ORM, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		fmtlog.Errorln("Error connect to database")
+	if DBtype == "mysql" {
+		dsn = fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local", db_username, db_password, db_host, db_database)
+		SELF.ORM, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err != nil {
+			fmtlog.Errorln("Error connect to database")
+			return err
+		}
+	} else if DBtype == "postgres" {
+		dsn = fmt.Sprintf("user=%s password=%s host=%s dbname=%s sslmode=disable", db_username, db_password, db_host, db_database)
+		SELF.ORM, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err != nil {
+			fmtlog.Errorln("Error connect to database")
+			return err
+		}
+	} else {
+		fmtlog.Errorln("Error database type")
+		return err
 	}
+
 	return err
 }
 
